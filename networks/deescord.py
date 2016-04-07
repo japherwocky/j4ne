072dd33b2e96f050e0bad6f1c7a8b852ac0dc689
japherwocky <japherwocky@charlotte>
2016-04-07 21:16:46 +0000

    retweeting

--- networks/deescord.py
+++ networks/deescord.py
@@ -3,6 +3,8 @@ from random import choice, shuffle
 from array import array
 import asyncio
 import re
+import os
+import json
 
 import discord
 from youtube_dl.utils import DownloadError
@@ -15,15 +17,17 @@ from websockets.exceptions import InvalidState
 
 from keys import discord_email, discord_pass
 
+# instantiate this here to use decorators
 client = discord.Client()  # loop defaults to asyncio.get_event_loop()
 
 
 class Discord(object):
     """ Mixin for the main App """
+    _Twitter = None
 
     @gen.coroutine
     def discord_connect(self):
-        self.DiscordParser = DiscordParser()  # not sure what magic actually hooks up the handlers
+        self.DiscordParser = parser # not sure what magic actually hooks up the handlers
 
         @client.event
         async def on_message(message):
@@ -41,6 +45,9 @@ class Discord(object):
     @client.event
     async def on_ready():
         info('Logged in as {} {}'.format(client.user.id, client.user.name) )
+
+        # try to load tweeters if they exist
+        await parser.load_twitter_config()
 
 
 
@@ -299,16 +306,16 @@ class DiscordParser(object):
             await asyncio.sleep(5)
             await client.send_message(message.channel, 'Done sleeping')
     
-        elif 'j4ne' in message.content and 'day' in message.content:
+        elif 'j4ne' in message.content.lower() and 'day' in message.content.lower():
             await self.on_triggered(message.channel)
    
-        elif message.content.startswith('|shrug'):
+        elif message.content.startswith('|shrug '):
             await self.say(message.channel, '`¯\_(ツ)_/¯`')
 
-        elif message.content.startswith('|shame'):
+        elif message.content.startswith('|shame '):
             await self.say('`ಠ_ಠ`')
 
-        elif message.content.startswith('|wizard'):
+        elif message.content.startswith('|wizard '):
 
             wizards = [
                 '`(∩｀-´)⊃━☆ﾟ.･｡ﾟ`',
@@ -354,3 +361,120 @@ class DiscordParser(object):
 
         elif message.content.startswith('|youropinion'):
             await self.say(message.channel, 'http://j4ne.pearachute.net/youropinion.png')
+
+        elif message.content.startswith('|retweet'):
+            await self.retweet(message)
+
+
+
+    async def retweet(self, message):
+
+        tooter = message.content.split('|retweet')[1]
+        if not tooter:
+            return await self.say(message.channel, 'Who should I retweet?')
+
+        tooter = tooter.strip()
+
+        this_server = message.server
+
+        conf = self._twitter_conf
+
+        if not this_server in conf:
+            conf[this_server] = {message.channel: []}
+
+        if not message.channel in conf[this_server]:
+            conf[this_server][message.channel] = []
+
+        if message.channel in conf[this_server] and tooter in [t['screen_name'] for t in conf[this_server][message.channel]]:
+            return await self.say(message.channel, 'I am already retweeting {} here.'.format(tooter))
+
+        conf[this_server][message.channel].append( {'screen_name':tooter, 'last':None})
+
+        await self.save_twitter_config()
+
+        await self.say(message.channel, "I will start retweeting {} in this channel.  Brace for incoming tweets.".format(tooter))
+
+        await self.check_tweets()
+
+
+    async def save_twitter_config(self):
+
+        out = {}
+        servers = {server.name:server for server in client.servers}
+
+        for serv in self._twitter_conf.keys():
+            out[serv.name] = {}
+       
+            for chann in self._twitter_conf[serv].keys():
+                out[serv.name][chann.name] = []
+
+                for tooter in self._twitter_conf[serv][chann]:
+                    out[serv.name][chann.name].append(tooter)
+                 
+        with open('./twitterconf.json', 'w') as f:
+            out = json.dumps(out, sort_keys=True, indent=4)
+
+            f.write(out)
+
+
+
+    async def load_twitter_config(self):
+
+        from networks.twatter import twitter
+        self._twitter = twitter
+
+        with open('./twitterconf.json') as f:
+            conf = json.loads(f.read())
+
+            # replace server strings with proper objects
+            servers = {server.name:server for server in client.servers}
+            for servstring in [k for k in conf.keys()]:
+                debug('Loading server {}'.format(servstring))
+
+                servobj = servers[servstring]
+                conf[servobj] = conf[servstring]
+                del conf[servstring]
+
+                for chanstring in [k for k in conf[servobj].keys()]:
+                    channels = {channel.name:channel for channel in servers[servstring].channels}
+                    chanobj = channels[chanstring]
+                    conf[servobj][chanobj] = conf[servobj][chanstring]
+                    del conf[servobj][chanstring]
+
+            self._twitter_conf = conf
+
+        # schedule polling for tweeters
+
+        info('Twitter config loaded, polling for new tweets...')
+        await self.check_tweets()
+        tornado.ioloop.PeriodicCallback(self.check_tweets, 5*60*1000).start()
+
+
+    async def check_tweets(self):
+
+        for serv in self._twitter_conf.keys():
+            debug(serv)
+
+            for chann in self._twitter_conf[serv].keys():
+                debug(chann)
+
+                for tooter in self._twitter_conf[serv][chann]:
+
+                    tweets = self._twitter.get_user_timeline(screen_name = tooter['screen_name'])
+                    tweets.reverse()
+
+                    for tweet in tweets:
+                        if tooter['last'] and tweet['id'] <= tooter['last']:
+                            continue
+
+                        if tweet['in_reply_to_status_id']:
+                            continue
+
+                        await self.say(chann, '{} tweets: {}'.format(tweet['user']['screen_name'],tweet['text']))
+
+                        tooter['last'] = tweet['id']
+
+        await self.save_twitter_config()
+
+
+parser = DiscordParser()  # not sure what magic actually hooks up the handlers
