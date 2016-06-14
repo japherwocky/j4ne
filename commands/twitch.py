c58efbe5acc497fd2a3a7c2ba594b9d6676e60e4
japherwocky@japhbookpro <github@pearachute.com>
2016-06-14 13:15:55 -0500

    owner only auth wrapper for twitch commands

--- commands/twitch.py
+++ commands/twitch.py
@@ -1,9 +1,27 @@
 from commands import twitch_command as command
+import logging
 
 from tornado.httpclient import HTTPError
 
+def auth_owner(func):
+    """
+    Decorator for registering commands
+    """
+
+    async def __decorator(network, channel, message):
+        # easy on twitch - check that the author of the message == the channel
+        author = message.author
+        if '#{}'.format(author.lower()) != channel:
+            await network.send_message(channel, 'Nice try, {}.'.format(message.author))
+
+        else:
+            return await func(network, channel, message)
+
+    return __decorator
+
 
 @command('mod')
+@auth_owner
 async def mod(network, channel, message):
 
     parts = message.content.split('mod',1)[1].strip().split(' ', 1)
@@ -14,6 +32,8 @@ async def mod(network, channel, message):
     # get user data from the API / check that it exists
     try:
         user = await network.application.TwitchAPI.query('https://api.twitch.tv/kraken/users/{}/'.format(parts[0]))
+        logging.info(user)
+        
     except HTTPError:
         return await network.send_message(channel, 'I could not find that user on twitch.')
         
