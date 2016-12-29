2a81035d024bd739ec54a29082730ec65feb0bb0
gitten <gitten@users.noreply.github.com>
2016-12-29 01:48:19 -0700

    wip

--- loggers/archive_models.py
+++ loggers/archive_models.py
@@ -1,16 +1,11 @@
-from peewee import SqliteDatabase
-from loggers.models as models
+from db import archive_db 
+import loggers.models as models
 
-from db import archive_db
+
 
 
 class Message(models.Message):
 
     class Meta:
         database = archive_db
-
-
-class Event(models.Event):
-
-    class Meta:
-        database = archive_db
+        db_table = 'messages'
--- scratch.py
+++ scratch.py
@@ -1,5 +1,8 @@
-from loggers.archive_models import Message, ArchiveMessage
+import loggers.models as models
 from db import db, archive_db
+import loggers.archive_models as archives
+
+from peewee import fn, IntegrityError
 from datetime import datetime, timedelta
 from loremipsum import get_sentences
 from random import randrange
@@ -34,3 +37,53 @@ def gen_batch_msg_data():
 def batch_insert(batch_data, model, database):
     with database.atomic():
         model.insert_many(batch_data).execute()
+
+
+def get_single(Model):
+    msgs = (Model
+            .select()
+            .group_by(Model)
+            .having(fn.Min(Model.timestamp))
+            .get())
+    return msgs
+
+
+def convert_timestamp(timestamp):
+    return datetime.fromtimestamp(timestamp)
+
+
+def is_archivable(timestamp):
+    delta = timedelta(days=2)
+    now = datetime.now()
+    return (now - timestamp) > delta
+
+
+def archivable_date_limit(days=2):
+    delta = timedelta(days=days)
+    return datetime.now() - delta
+
+
+
+
+def archive(cutoff_period=2, rows=3):
+    cutoff = datetime.now() - timedelta(days=cutoff_period)
+    msgs = (models.Message
+            .select()
+            .where(models.Message.timestamp < cutoff)
+            .limit(rows))
+
+    try:
+        with archive_db.atomic():
+            archive_db.connect()
+            archives.Message.insert_many(msgs.dicts()).execute()
+            archive_db.close()
+
+            with db.atomic():
+                for row in msgs:
+                    (models.Message
+                     .delete()
+                     .where(models.Message.id == row.id))
+        return 'Archive successful.'
+
+    except IntegrityError:
+        return 'Archiving failed: Records may already exist in archive.'
