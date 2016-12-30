dc7326b2e6ed38db292f21a5d855696982c8e729
gitten <gitten@users.noreply.github.com>
2016-12-30 00:18:29 -0700

    added table name to meta

--- loggers/archive_models.py
+++ loggers/archive_models.py
@@ -1,11 +1,14 @@
-from db import archive_db 
+from db import archive_db
 import loggers.models as models
 
 
-
-
 class Message(models.Message):
-
     class Meta:
         database = archive_db
         db_table = 'messages'
+
+
+class Events(models.Event):
+    class Meta:
+        database = archive_db
+        db_table = 'events'
