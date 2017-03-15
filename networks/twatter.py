609e837627e4f6419a46d2b395d7b58c2f6b7f42
gitten <gitten@users.noreply.github.com>
2017-03-15 02:28:14 -1000

    DiscordServer table no longer needed

--- networks/twatter.py
+++ networks/twatter.py
@@ -7,7 +7,7 @@ from twython import Twython,TwythonStreamer
 from keys import twitter_appkey, twitter_appsecret, twitter_token, twitter_tokensecret
 from logging import debug, info, warning
 
-from networks.models import DiscordServer, DiscordChannel, Tooter
+from networks.models import DiscordChannel, Tooter
 
 # twitter = Twython(twitter_appkey, twitter_appsecret, twitter_token, twitter_tokensecret)
 # twitter.verify_credentials()
